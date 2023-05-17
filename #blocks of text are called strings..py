de0734c21f6d3211104bd517106019bc1f794683
alexandre correia <alexandrecramos95@gmail.com>
2023-05-17 11:00:34 -0700

    test

---
+++ #blocks of text are called strings..py
@@ -0,0 +1,10 @@
+#blocks of text are called strings.
+#double or single quotes can be used. be consistent with whichever one you choose
+print("Hello World!")
+
+#Variables are a way of storing data. Below we will create a variable and store a value
+#We assign a value by using =
+#variable is "meal" and the value is "an english muffin"
+meal = "An english muffin"
+
+
