d1201018862ab32e896233823a1c6b280e7c3744
alexandre correia <alexandrecramos95@gmail.com>
2023-05-25 10:26:52 -0700

    updates

--- combobox1.py
+++ combobox1.py
@@ -10,6 +10,18 @@ def on_select(event):
     selected_item = event.widget.get()
     print("Selected Item:", selected_item)
     
-#
+#creating root window
 root = tk.Tk()
 root.title("Combobox Example")
+
+#creating static array
+items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+
+#creating widget object
+combo_box = ttk.Combobox(root, values=items)
+#obtaining event from combo box object
+combo_box.bind("<<ComboboxSelected>>", on_select)
+
+combo_box.pack()
+#run mainloop to keep window open
+root.mainloop()
